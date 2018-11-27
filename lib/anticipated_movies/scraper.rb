b009893fe1d6e9ce55a17abcb2699b0fbf91ebe9
thisisSDK <25039980+thisisSDK@users.noreply.github.com>
2018-11-26 21:28:04 -0500

    narrows movie list to 72 options - removes blank entries

--- lib/anticipated_movies/scraper.rb
+++ lib/anticipated_movies/scraper.rb
@@ -4,19 +4,15 @@ class AnticipatedMovies::Scraper
     html = open ("https://editorial.rottentomatoes.com/article/most-anticipated-2019-movies/")
 
     doc = Nokogiri::HTML(html)
-
-    doc.css(".articleContentBody").each do |movie_doc|
-      #binding.pry
+    doc.css("strong a").each do |movie_doc|
       #title = movie_doc.text
-      title = movie_doc.css("strong a").text #tried this when attempting to pull URL
-      url = movie_doc.css("strong a").attribute("href").value #pulls value when binding.pry is commented in on line 10. 
+      title = movie_doc.text #tried this when attempting to pull URL
+      url = movie_doc.attribute("href").value #pulls value when binding.pry is commented in on line 10.
       #movie = AnticipatedMovies::MovieDetails.new(title)
       movie = AnticipatedMovies::MovieDetails.new(title, url) #comment back in and delete previous line when pulling the URL
 
       movie.save
-      #binding.pry
-
     end
-
+    #binding.pry
   end
 end
